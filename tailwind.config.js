f402260ff649fdc724c3d5f31863ac6f51cee67e
nandkishorr <rachulr7@gmail.com>
2024-12-16 14:13:12 +0530

    tailwind and folder stucture setup completed

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import tailwindScrollbar from 'tailwind-scrollbar';
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindScrollbar({ nocompatible: true }),
+  ],
+}
