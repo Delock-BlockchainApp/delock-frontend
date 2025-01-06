88c3b9d32516eb7d970e32d621b3586541b33407
Aparna Pradeep <aparnapradeep31@gmail.com>
2025-01-06 11:05:46 +0530

    Your docs

---
+++ src/components/yourdocs_card.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+interface YourDocsCardProps {
+    title: string;
+    description: string;
+    Authority: string;
+}
+
+const yourdocs_card: React.FC<YourDocsCardProps> = ({ title, description, Authority }) => {
+    return (
+       
+    
+
+    
+        <div className=' flex  justify-between rounded-xl bg-white pt-1 w-2/3 h-14 mt-5 ml-5 [box-shadow:4px_4px_10px_rgba(0,0,0,0.2)]'>
+            <div className=' ml-5'>
+                <div className=' font-poppins ml-4  text-base mt-1 pb-0'> {title}</div>
+
+
+                <div className=' font-poppins ml-4 mt-0 pt-0 '>{description}</div>
+            </div>
+            <div className='w-1/2 flex justify-center items-center font-poppins   text-sm opacity-50'>
+                {Authority}
+            </div>
+            <div className=' flex justify-center items-center font-poppins  text-sm'>
+                <div className='flex flex-col justify-center items-center '>
+
+                    <i className="fa-solid fa-up-right-from-square text-xl " style={{ color: '#022A51' }}></i>
+                    <div className='font-poppins   ' style={{ color: '#022A51' }}>
+                        Preview
+                    </div>
+                </div>
+                <div className='flex flex-col justify-center items-center ml-4 mr-8'>
+                    <i className="fa-solid fa-cloud-arrow-down text-xl " style={{ color: '#022A51' }}></i>
+                    <div className='font-poppins   ' style={{ color: '#022A51' }}>
+                        Download
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        
+        
+    )
+}
+
+export default yourdocs_card
--- src/pages/Yourdocs.tsx
+++ src/pages/Yourdocs.tsx
@@ -1,8 +1,26 @@
+import Yourdocs_card from "../components/yourdocs_card"
+
 function Yourdocs() {
   return (
     <div>
-      yourdocs
+    
+      <div className=" text-5xl font-poppins font-semibold mt-2 mb-6" style={{ color: '#004182' }}>Your Docs</div>
+      <div className=" text-sm font-poppins font-bold  ml-5 ">12 Issued documents Found</div>
+      <div className=" flex flex-col align">
+      <Yourdocs_card title={'Aadhaar Card' } description={'**************'} Authority={'Unique Identification Authority of India(UIDAI)'}/>
+      <Yourdocs_card title={'Driving License' } description={'KL05*******15'} Authority={'Motor Vechile Department,Kerala'}/>
+      <Yourdocs_card title={'PAN Verification' } description={'FUE******9A'} Authority={'Income Tax Department'}/>
+      <Yourdocs_card title={'Class X Marksheet' } description={'431******2019'} Authority={'Central Board of Secondary Education'}/>
+      <Yourdocs_card title={'Registraton of Vechiles' } description={'KL01*******189'} Authority={'Motor Vehicle Department, Kerala'}/>
+      </div>
     </div>
+      
+      
+
+      
+    
+      
+    
   )
 }
 
