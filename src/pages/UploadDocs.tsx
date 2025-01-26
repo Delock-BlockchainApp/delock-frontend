1d36426cf2322fdb0ab6e40f50b467ca150aba15
nandkishorr <rachulr7@gmail.com>
2025-01-26 18:54:26 +0530

    feat:upload page updated

--- src/pages/UploadDocs.tsx
+++ src/pages/UploadDocs.tsx
@@ -4,8 +4,15 @@ function UploadDocs() {
   return (
     <div className="h-full p-5 overflow-y-scroll scrollbar">
       {/* Top section */}
-      <div>
+      <div className="flex justify-between">
         <TextComponent text="Upload Docs" fontSize="40px" />
+        <div className=" w-10 h-10 rounded-full bg-[#004182] flex items-center justify-center" >
+            <i className="fa-regular fa-user text-white text-base "></i>
+          </div>
+      </div>
+      {/* form area */}
+      <div className="mt-5 flex"> 
+        <div className="flex h-9 w-52 rounded-lg bg-bold-blue text-white justify-center items-center font-semibold">Driving License</div>
       </div>
     </div>
   )
